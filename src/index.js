0b5af805946a2fdd424ab90762463f6ff9dc9a25
hayleamlak <hayleamlaktesfaye12@gmail.com>
2025-12-02 11:30:34 +0300

    add my own feature -2 next to you

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ console.log('index.js');
 console.log('version13');
 console.log('feature-A form hayleamalk');
 console.log('feature-A form hayleamalk to kirubel');
-
+console.log('feature-2 form hayleamalk to kirubel');
